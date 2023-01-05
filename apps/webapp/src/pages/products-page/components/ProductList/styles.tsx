4164952c9dcc9786f2943fa258315e6ffe0a6c07
Valentina Garavaglia <vaahxx@gmail.com>
2023-01-05 12:37:10 -0500

    feat: add responsive design

--- apps/webapp/src/pages/products-page/components/ProductList/styles.tsx
+++ apps/webapp/src/pages/products-page/components/ProductList/styles.tsx
@@ -1,9 +1,14 @@
 import { Theme } from "@mui/material"
+import { pink } from "@mui/material/colors"
 import { makeStyles } from "@mui/styles"
 
-export const useProductListStyles = makeStyles(() => ({
+export const useProductListStyles = makeStyles((theme: Theme) => ({
   cardsContainer: {
     display: "flex",
     gap: "1.5rem",
+    flexDirection: "column",
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
   },
 }))
