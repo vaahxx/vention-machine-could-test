08dc62cde67a7dc84d3c30f73bad89599e8e510f
Valentina Garavaglia <vaahxx@gmail.com>
2023-01-07 10:48:40 -0500

    fix: fixed init.sql

--- apps/database/postgres/init.sql
+++ apps/database/postgres/init.sql
@@ -1,27 +1,19 @@
 create database "vention_machine_cloud_test";
 
 create table product (
-  name varchar(255),
-  image varchar(255),
-  price int,
-  in_cart boolean
+  id serial4 not null,
+  createdAt timestamp not null default now(),
+  updatedAt timestamp not null default now(),
+  name varchar(255) not null,
+  image varchar(255) not null,
+  price int not null,
+  in_cart boolean not null
 );
 
 create table rating (
-  product_id int,
-  value int
+  id serial4 not null,
+  createdAt timestamp not null default now(),
+  updatedAt timestamp not null default now(),
+  product_id int not null,
+  value int not null
 );
-
-insert into product (name, image, price, in_cart)
-values
-('Red flower', 'https://www.freepnglogos.com/uploads/flour-png/flour-flower-png-transparent-png-images-pluspng-2.png', 20, TRUE),
-('Peach flower', 'https://www.freepnglogos.com/uploads/flour-png/flour-peach-flower-png-16.png', 15, FALSE),
-('Pink flower', 'https://www.freepnglogos.com/uploads/flour-png/flour-flower-images-transparent-background-19.png', 10, TRUE);
-
-insert into rating (product_id, value)
-values (1, 3),
-(1, 4),
-(1, 5),
-(2, 4),
-(2, 5),
-(3, 2);
